ba63323a061e941e0b2397c15861fc9bc57acf34
Sophia Chow <Sophia.t.chow@gmail.com>
2017-10-26 19:54:15 -0500

    Soph finished atm-react hw

--- src/Account.js
+++ src/Account.js
@@ -1,14 +1,56 @@
 import React, { Component } from 'react';
 
 export default class Account extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      balance: 0
+    };
+
+    this.handleDepositClick = this.handleDepositClick.bind(this);
+    this.handleWithdrawClick = this.handleWithdrawClick.bind(this);
+  }
+
+  handleDepositClick(e){
+    console.log("handleDepositClick");
+    e.preventDefault();
+    let amount = parseFloat(this.inputBox.value);
+    let newBalance = this.state.balance + amount;
+      this.setState({
+        balance: newBalance
+      });
+    this.inputBox.value = '';
+  }
+  handleWithdrawClick(e){
+    console.log("handleWithdrawlClick");
+    e.preventDefault();
+    let amount = parseFloat(this.inputBox.value);
+
+    if(amount < this.state.balance){
+    let newBalance = this.state.balance - amount;
+
+      this.setState({
+        balance: newBalance
+      });
+    this.inputBox.value = '';
+    }else{
+    alert("You do not have sufficient funds!");
+  }
+  }
+
   render() {
+    let balanceClass = 'balance';
+    if(this.state.balance === 0){
+      balanceClass += ' zero';
+    }
     return (
       <div className="account">
-        <h2>TODO: CHANGE TO PROP</h2>
-        <div className="balance">$0</div>
-        <input type="text" placeholder="enter an amount" />
-        <input type="button" value="Deposit" />
-        <input type="button" value="Withdraw" />
+        <h2>{this.props.name}</h2>
+        <div className={balanceClass}>{this.state.balance}</div>
+        <input type="text" placeholder="enter an amount" ref={(input)=>this.inputBox = input} />
+        <input type="button" value="Deposit" onClick={(e)=>this.handleDepositClick(e)} />
+        <input type="button" value="Withdraw" onClick={(e)=>this.handleWithdrawClick(e)} />
       </div>
     )
   }
--- src/App.js
+++ src/App.js
@@ -11,9 +11,9 @@ class App extends Component {
           <div id="logo"><img src={logo} alt="General Assembly logo" /></div>
           <div id="title">Bank of GA</div>
         </div>
-        <Account />
-        <Account />
-        <div className="clear"></div>
+          <Account name="Checking"/>
+          <Account name="Savings" />
+          <div className="clear"></div>
       </div>
     );
   }
